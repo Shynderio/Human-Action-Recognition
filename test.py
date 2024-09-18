4dff35fc1dac4a75d92549eace3ea982754991fe
Nguyen Duc Thinh <97095536+Shynderio@users.noreply.github.com>
2024-09-19 01:11:28 +0700

    test lstm

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from src.lstm import ActionClassificationLSTM
 from src.video_analyzer_web import analyse_video
 
 # import some common Detectron2 utilities
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
+#from detectron2 import model_zoo
+#from detectron2.engine import DefaultPredictor
+#from detectron2.config import get_cfg
 import time
 import cv2
 import numpy as np
@@ -150,17 +150,17 @@ def analyse_video(pose_detector, lstm_classifier, video_path):
     analyze_done = time.time()
     print("Video processing finished in ", analyze_done - start)
 
-start = time.time()
+#start = time.time()
 # obtain detectron2's default config
-cfg = get_cfg()
+#cfg = get_cfg()
 # load the pre trained model from Detectron2 model zoo
-cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
+#cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml"))
 # set confidence threshold for this model
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+#cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 # load model weights
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
+#cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_3x.yaml")
 
-cfg.MODEL.DEVICE = "cuda:0"
+#cfg.MODEL.DEVICE = "cuda:0"
 # create the predictor for pose estimation using the config
 #pose_detector = DefaultPredictor(cfg)
 
@@ -168,7 +168,7 @@ cfg.MODEL.DEVICE = "cuda:0"
 
 
 
-model_load_done = time.time()
+#model_load_done = time.time()
 
 
 lstm_classifier = ActionClassificationLSTM.load_from_checkpoint("models/epoch=394-step=17774.ckpt")
